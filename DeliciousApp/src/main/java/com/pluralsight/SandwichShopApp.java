1bdc61e4f832193692f7f89c010a09e9634a0d72
Bilenie Tilahun <bilumekb@gmail.com>
2025-05-22 14:42:25 -0500

    rename my project and main class name.

--- DeliciousApp/src/main/java/com/pluralsight/SandwichShopApp.java
+++ DeliciousApp/src/main/java/com/pluralsight/SandwichShopApp.java
@@ -1,6 +1,6 @@
 package com.pluralsight;
 
-public class SandwichShop {
+public class SandwichShopApp {
     public static void main(String[] args) {
         System.out.println("Hello Beautiful people!!!");
 
