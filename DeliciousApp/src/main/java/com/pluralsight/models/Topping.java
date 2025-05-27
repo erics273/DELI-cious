66f9722d06f6940254afa1ee9d730e9fef910bf5
Bilenie Tilahun <bilumekb@gmail.com>
2025-05-27 11:52:20 -0500

    update code.

--- DeliciousApp/src/main/java/com/pluralsight/models/Topping.java
+++ DeliciousApp/src/main/java/com/pluralsight/models/Topping.java
@@ -1,7 +1,7 @@
 package com.pluralsight.models;
 
 public interface Topping {
-    // Abstract method to be implemented by all classes that represent sandwich toppings
+    // Abstract method to be implemented by all classes that represent sandwich toppings=> regular and prem- price calculate size => get price and summary
     String getType();
 
     boolean isExtra();
